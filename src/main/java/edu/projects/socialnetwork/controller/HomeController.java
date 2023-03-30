abee7c8da7faf028de341ce4ecb8ffe2e7cd9810
erick <erickgoncalves5@hotmail.com>
2023-03-30 17:24:01 -0300

    Succesfully pass posts to home.html, still need to work the like form

--- src/main/java/edu/projects/socialnetwork/controller/HomeController.java
+++ src/main/java/edu/projects/socialnetwork/controller/HomeController.java
@@ -1,13 +1,25 @@
 package edu.projects.socialnetwork.controller;
 
+import edu.projects.socialnetwork.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
+    private final PostService postService;
+
+    @Autowired
+    public HomeController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping(path = "/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("posts", postService.getPosts());
         return "home";
     }
 }
